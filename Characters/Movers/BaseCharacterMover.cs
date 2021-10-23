562f682d4e8f1cda63f207a8db79bcdfef037e9a
Colin2222 <crlew@outlook.com>
2021-10-23 14:05:58 -0400

    placeholder mover
    -empty character mover to test direction

---
+++ Characters/Movers/BaseCharacterMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterMover : MonoBehaviour
+{
+    public CharacterScript characterScript;
+
+    [System.NonSerialized]
+    public int direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
