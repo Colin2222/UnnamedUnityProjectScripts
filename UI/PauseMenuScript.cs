d42be84bc29a940f9178d859b3a23fafd3acac3d
Colin2222 <crlew@outlook.com>
2021-10-23 13:59:30 -0400

    Initial pause menu implementation
    -Created pause menu
    -Stops game with menu/start button
    -Contains nonfunctional resume and quit buttons

---
+++ UI/PauseMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject menuCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Menu")){
+            if(gamePaused){
+                Resume();
+            } else{
+                Pause();
+            }
+        }
+    }
+
+    private void Resume(){
+        menuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    private void Pause(){
+        menuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+}
